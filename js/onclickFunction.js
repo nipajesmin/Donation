19ceed9e823be1080ec2df153eea31f2b372ec50
nipajesmin <nipakterjesmin@gmail.com>
2024-09-24 01:30:52 +0600

    add history and donation button features

---
+++ js/onclickFunction.js
@@ -0,0 +1,22 @@
+document.getElementById('donation-btn')
+    .addEventListener('click', function () {
+        showSectionById('donation-part');
+
+        // Change the background color of the clicked button
+    document.getElementById('donation-btn').style.backgroundColor = 'rgb(180, 244, 97)';
+    
+    // Reset the background color of the history button
+    document.getElementById('history-btn').style.backgroundColor = '';
+    });
+
+
+document.getElementById('history-btn')
+    .addEventListener('click', function () {
+        showSectionById('history-part');
+
+        // Change the background color of the clicked button
+    document.getElementById('history-btn').style.backgroundColor = 'rgb(180, 244, 97)';
+    
+    // Reset the background color of the donation button
+    document.getElementById('donation-btn').style.backgroundColor = '';
+    });
--- js/sharedFunction.js
+++ js/sharedFunction.js
@@ -12,3 +12,11 @@ function getTextFieldValueById(id){
     return textNumber;
 }
 
+function showSectionById(id){
+    // hide all the sections
+    document.getElementById('donation-part').classList.add('hidden');
+    document.getElementById('history-part').classList.add('hidden');
+    
+    // show the section with the provide id as parameter
+    document.getElementById(id).classList.remove('hidden');
+}
